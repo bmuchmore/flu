4ba4b4cf5e82153ab6e16672125d5a4b282f664f
Brian Muchmore <bmuchmore@gmail.com>
2019-11-08 20:38:59 -0500

    .
    sessionInfo:
    R version 3.6.1 (2019-07-05)
    Platform: x86_64-pc-linux-gnu (64-bit)
    Running under: Ubuntu 18.04.3 LTS
    
    Matrix products: default
    BLAS:   /usr/lib/x86_64-linux-gnu/blas/libblas.so.3.7.1
    LAPACK: /usr/lib/x86_64-linux-gnu/lapack/liblapack.so.3.7.1
    
    locale:
     [1] LC_CTYPE=en_US.UTF-8       LC_NUMERIC=C               LC_TIME=en_US.UTF-8        LC_COLLATE=en_US.UTF-8    
     [5] LC_MONETARY=en_US.UTF-8    LC_MESSAGES=en_US.UTF-8    LC_PAPER=en_US.UTF-8       LC_NAME=C                 
     [9] LC_ADDRESS=C               LC_TELEPHONE=C             LC_MEASUREMENT=en_US.UTF-8 LC_IDENTIFICATION=C       
    
    attached base packages:
    [1] stats     graphics  grDevices utils     datasets  methods   base     
    
    other attached packages:
     [1] trelliscopejs_0.1.19     microbenchmark_1.4-7     foreach_1.4.7            zipcode_1.0             
     [5] tidycensus_0.9.2         widgetframe_0.3.1        htmlwidgets_1.5.1        rpivotTable_0.3.0       
     [9] manipulateWidget_0.10.0  lubridate_1.7.4          labelled_2.2.1           DT_0.9                  
    [13] htmltools_0.4.0          leaflet.extras_1.0.0     formr_0.8.4              leaflet.minicharts_0.5.4
    [17] leafsync_0.1.0           magrittr_1.5             crosstalk_1.0.1.9000     forcats_0.4.0           
    [21] dplyr_0.8.3              purrr_0.3.3              readr_1.3.1              tidyr_1.0.0             
    [25] tibble_2.1.3             ggplot2_3.2.1            tidyverse_1.2.1          sf_0.8-0                
    [29] stringr_1.4.0            leaflet_2.0.2            flexdashboard_0.5.1.1    git2r_0.26.1            
    
    loaded via a namespace (and not attached):
     [1] colorspace_1.4-1        ellipsis_0.3.0          class_7.3-15            mclust_5.4.5           
     [5] rgdal_1.4-6             base64enc_0.1-3         rstudioapi_0.10         xml2_1.2.2             
     [9] codetools_0.2-16        knitr_1.25              zeallot_0.1.0           jsonlite_1.6           
    [13] packrat_0.5.0           broom_0.5.2             shiny_1.4.0             compiler_3.6.1         
    [17] httr_1.4.1              backports_1.1.5         assertthat_0.2.1        fastmap_1.0.1          
    [21] lazyeval_0.2.2          cli_1.1.0               later_1.0.0             prettyunits_1.0.2      
    [25] tools_3.6.1             gtable_0.3.0            glue_1.3.1              rappdirs_0.3.1         
    [29] Rcpp_1.0.2              cellranger_1.1.0        vctrs_0.2.0             nlme_3.1-141           
    [33] blogdown_0.16           autocogs_0.1.2          tigris_0.8.2            iterators_1.0.12       
    [37] xfun_0.10               rvest_0.3.4             mime_0.7                miniUI_0.1.1.1         
    [41] lifecycle_0.1.0         scales_1.0.0            DistributionUtils_0.6-0 hms_0.5.2              
    [45] promises_1.1.0          RColorBrewer_1.1-2      yaml_2.2.0              curl_4.2               
    [49] gridExtra_2.3           stringi_1.4.3           maptools_0.9-5          checkmate_1.9.4        
    [53] e1071_1.7-2             rlang_0.4.1             pkgconfig_2.0.3         evaluate_0.14          
    [57] lattice_0.20-38         labeling_0.3            tidyselect_0.2.5        bookdown_0.14          
    [61] R6_2.4.0                generics_0.0.2          DBI_1.0.0               pillar_1.4.2           
    [65] haven_2.1.1             foreign_0.8-72          withr_2.1.2             units_0.6-5            
    [69] sp_1.3-1                modelr_0.1.5            crayon_1.3.4            uuid_0.1-2             
    [73] KernSmooth_2.23-15      rmarkdown_1.16          progress_1.2.2          viridis_0.5.1          
    [77] grid_3.6.1              readxl_1.3.1            digest_0.6.22           classInt_0.4-1         
    [81] webshot_0.5.1           xtable_1.8-4            httpuv_1.5.2            munsell_0.5.0          
    [85] viridisLite_0.3.0

--- scripts/build_and_push.R
+++ scripts/build_and_push.R
@@ -1,28 +1,28 @@
-## https://popgen.nescent.org/CONTRIBUTING_WITH_GIT2R.html
-## https://github.com/settings/tokens
-library(git2r)
-
-# system(
-#   'echo brian | sudo -S echo ',
-#   intern = FALSE,
-#   ignore.stdout = FALSE,
-#   ignore.stderr = FALSE,
-#   wait = TRUE,
-#   input = NULL,
-#   timeout = 0
-# )
-
-rmarkdown::render("/home/brian/Desktop/flu/static/flu_dashboard.Rmd")
-
-repo <- repository(path = "/home/brian/Desktop/flu", discover = TRUE)
-remote_add(repo, name = "flu", url = "https://github.com/bmuchmore/flu.git")
-remotes(repo)
-checkout(repo, "master")
-fetch(repo, name = "flu")
-merge(repo, "flu/master")
-git2r::add(repo, "*")
-status(repo)
-commit(repo, message = ".", all = TRUE, session = TRUE)
-cred <- cred_token()
-push(repo, credentials = cred)
-      
+  ## https://popgen.nescent.org/CONTRIBUTING_WITH_GIT2R.html
+  ## https://github.com/settings/tokens
+  library(git2r)
+  
+  # system(
+  #   'echo brian | sudo -S echo ',
+  #   intern = FALSE,
+  #   ignore.stdout = FALSE,
+  #   ignore.stderr = FALSE,
+  #   wait = TRUE,
+  #   input = NULL,
+  #   timeout = 0
+  # )
+  
+  rmarkdown::render("/home/brian/Desktop/flu/static/flu_dashboard.Rmd")
+  
+  repo <- repository(path = "/home/brian/Desktop/flu", discover = TRUE)
+  remote_add(repo, name = "flu", url = "https://github.com/bmuchmore/flu.git")
+  remotes(repo)
+  checkout(repo, "master")
+  fetch(repo, name = "flu")
+  merge(repo, "flu/master")
+  git2r::add(repo, "*")
+  status(repo)
+  commit(repo, message = ".", all = TRUE, session = TRUE)
+  cred <- cred_token()
+  push(repo, credentials = cred)
+        
