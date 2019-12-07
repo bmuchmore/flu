aa218ee14cc831bef5cff78877fe9bc2b54b35dc
Brian Muchmore <bmuchmore@gmail.com>
2019-12-07 16:45:21 -0500

    .
    sessionInfo:
    R version 3.6.1 (2019-07-05)
    Platform: x86_64-pc-linux-gnu (64-bit)
    Running under: Ubuntu 18.04.3 LTS
    
    Matrix products: default
    BLAS:   /usr/lib/x86_64-linux-gnu/blas/libblas.so.3.7.1
    LAPACK: /usr/lib/x86_64-linux-gnu/lapack/liblapack.so.3.7.1
    
    locale:
     [1] LC_CTYPE=en_US.UTF-8       LC_NUMERIC=C               LC_TIME=en_US.UTF-8       
     [4] LC_COLLATE=en_US.UTF-8     LC_MONETARY=en_US.UTF-8    LC_MESSAGES=en_US.UTF-8   
     [7] LC_PAPER=en_US.UTF-8       LC_NAME=C                  LC_ADDRESS=C              
    [10] LC_TELEPHONE=C             LC_MEASUREMENT=en_US.UTF-8 LC_IDENTIFICATION=C       
    
    attached base packages:
    [1] stats     graphics  grDevices utils     datasets  methods   base     
    
    other attached packages:
     [1] heatmaply_0.16.0         viridis_0.5.1            viridisLite_0.3.0       
     [4] plotly_4.9.0             ggplot2_3.2.1            rpivotTable_0.3.0       
     [7] lubridate_1.7.4          labelled_2.2.1           htmltools_0.4.0         
    [10] readr_1.3.1              leaflet.extras_1.0.0     formr_0.8.4             
    [13] tidyr_1.0.0              leaflet.minicharts_0.5.4 magrittr_1.5            
    [16] sf_0.8-0                 leaflet_2.0.2            flexdashboard_0.5.1.1   
    [19] git2r_0.26.1             pins_0.2.0               stringr_1.4.0           
    [22] dplyr_0.8.3              purrr_0.3.3              rowr_1.1.3              
    [25] shinyWidgets_0.5.0       shiny_1.4.0              DT_0.9                  
    
    loaded via a namespace (and not attached):
     [1] colorspace_1.4-1     ellipsis_0.3.0       class_7.3-15         rsconnect_0.8.15    
     [5] base64enc_0.1-3      rstudioapi_0.10      farver_2.0.1         fansi_0.4.0         
     [9] codetools_0.2-16     knitr_1.25           zeallot_0.1.0        jsonlite_1.6        
    [13] cluster_2.1.0        compiler_3.6.1       httr_1.4.1           backports_1.1.5     
    [17] assertthat_0.2.1     fastmap_1.0.1        lazyeval_0.2.2       cli_1.1.0           
    [21] later_1.0.0          sourcetools_0.1.7    tools_3.6.1          gtable_0.3.0        
    [25] glue_1.3.1           reshape2_1.4.3       rappdirs_0.3.1       Rcpp_1.0.3          
    [29] vctrs_0.2.0          gdata_2.18.0         iterators_1.0.12     crosstalk_1.0.1.9000
    [33] xfun_0.10            mime_0.7             lifecycle_0.1.0      gtools_3.8.1        
    [37] dendextend_1.12.0    MASS_7.3-51.4        scales_1.1.0         TSP_1.1-7           
    [41] hms_0.5.2            promises_1.1.0       RColorBrewer_1.1-2   yaml_2.2.0          
    [45] curl_4.2             gridExtra_2.3        stringi_1.4.3        gclus_1.3.2         
    [49] foreach_1.4.7        e1071_1.7-2          seriation_1.2-8      caTools_1.17.1.2    
    [53] rlang_0.4.2          pkgconfig_2.0.3      bitops_1.0-6         evaluate_0.14       
    [57] htmlwidgets_1.5.1    labeling_0.3         tidyselect_0.2.5     plyr_1.8.4          
    [61] R6_2.4.1             gplots_3.0.1.1       DBI_1.0.0            pillar_1.4.2        
    [65] haven_2.1.1          withr_2.1.2          units_0.6-5          tibble_2.1.3        
    [69] crayon_1.3.4         KernSmooth_2.23-15   utf8_1.1.4           rmarkdown_1.16      
    [73] grid_3.6.1           data.table_1.12.2    forcats_0.4.0        digest_0.6.23       
    [77] classInt_0.4-1       webshot_0.5.1        xtable_1.8-4         httpuv_1.5.2        
    [81] munsell_0.5.0        registry_0.5-1

--- scripts/build_and_push.R
+++ scripts/build_and_push.R
@@ -1,29 +1,27 @@
-## https://popgen.nescent.org/CONTRIBUTING_WITH_GIT2R.html
-## https://github.com/settings/tokens
-library(git2r)
+  ## https://popgen.nescent.org/CONTRIBUTING_WITH_GIT2R.html
+  ## https://github.com/settings/tokens
+  library(git2r)
+  
+  system(
+    'Rscript "/home/brian/Desktop/flu/static/census_trelliscope.R"',
+    intern = FALSE,
+    ignore.stdout = FALSE,
+    ignore.stderr = FALSE,
+    wait = TRUE,
+    input = NULL,
+    timeout = 0
+  )
+  
+  rmarkdown::render("/home/brian/Desktop/flu/static/flu_dashboard.Rmd")
+  repo <- repository(path = "/home/brian/Desktop/flu", discover = TRUE)
+  remote_add(repo, name = "flu", url = "https://github.com/bmuchmore/flu.git")
+  remotes(repo)
+  checkout(repo, "master")
+  fetch(repo, name = "flu")
+  merge(repo, "flu/master")
+  git2r::add(repo, "*")
+  status(repo)
+  commit(repo, message = ".", all = TRUE, session = TRUE)
+  cred <- cred_token()
+  push(repo, credentials = cred)
 
-system(
-  'Rscript "/home/brian/Desktop/flu/static/census_trelliscope.R"',
-  intern = FALSE,
-  ignore.stdout = FALSE,
-  ignore.stderr = FALSE,
-  wait = TRUE,
-  input = NULL,
-  timeout = 0
-)
-
-rmarkdown::render("/home/brian/Desktop/flu/static/flu_dashboard.Rmd")
-repo <- repository(path = "/home/brian/Desktop/flu", discover = TRUE)
-remote_add(repo, name = "flu", url = "https://github.com/bmuchmore/flu.git")
-remotes(repo)
-checkout(repo, "master")
-fetch(repo, name = "flu")
-merge(repo, "flu/master")
-git2r::add(repo, "*")
-status(repo)
-commit(repo, message = ".", all = TRUE, session = TRUE)
-cred <- cred_token()
-push(repo, credentials = cred)
-
-library(DT)
-DT::tableHeader()
